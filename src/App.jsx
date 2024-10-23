17433192ff3f4c45138766b0c6202f530defb6b4
Muhammet Furkan Kayıkcı <mfurkankayikci@gmail.com>
2024-10-23 19:02:02 +0300

    init project

---
+++ src/App.jsx
@@ -0,0 +1,3 @@
+const App = () => <div>Movie App</div>;
+
+export default App;
---
+++ src/features/movieSlice.js
@@ -0,0 +1,38 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchMovies = createAsyncThunk(
+  "movies/fetchMovies",
+  async (searchTerm = "Pokemon") => {
+    const response = await axios.get(
+      `http://www.omdbapi.com/?s=${searchTerm}&apikey=YOUR_API_KEY`
+    );
+    return response.data.Search; // Film listesini döner
+  }
+);
+
+const movieSlice = createSlice({
+  name: "movies",
+  initialState: {
+    movies: [],
+    status: "idle",
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMovies.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchMovies.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.movies = action.payload;
+      })
+      .addCase(fetchMovies.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default movieSlice.reducer;
